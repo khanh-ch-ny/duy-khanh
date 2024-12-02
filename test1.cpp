4c8622239ccbfcc59b45bc27baa06b46b1449f74
khanh-ch-ny <khanh040605@gmail.com>
2024-12-02 22:14:26 +0700

    add file test1.cpp

---
+++ test1.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+using namespace name;
+
+class SV
+{
+private:
+  string name;
+  int age;
+  string id;
+public;
+
+   void themsinhvien();
+   void xoasinhvien(string msv);
+   void suasinhvien(string msv);
+   void xemsinhvien();
+};
+    SV::SV(string ten,int tuoi,string msv){
+        name = ten;
+        age = tuoi;
+        id = tuoi;
+    }
+int main(){
+    int nhap;
+    string id;
+    SV sv;
+    
+
